e3037773ad01732f0590a9a319faf67a382fff77
Kota Iwasaki <s24C1017db@s.chibakoudai.jp>
2025-12-29 19:29:42 +0900

    create a file

---
+++ launch/system.monitor.py
@@ -0,0 +1,18 @@
+from launch 
+import LaunchDescription
+from launch_ros.actions 
+import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='mypkg',
+            executable='talker',
+            name='cpu_talker'
+        ),
+        Node(
+            package='mypkg',
+            executable='listener',
+            name='cpu_listener'
+        ),
+    ])

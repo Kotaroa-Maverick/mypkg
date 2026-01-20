65f6d09f8b7f1f09d706610a8bf26096691e80a0
Kota Iwasaki <s24C1017db@s.chibakoudai.jp>
2026-01-21 03:09:21 +0900

    change

--- test/test.bash
+++ test/test.bash
@@ -2,17 +2,14 @@
 # SPDX-FileCopyrightText: 2026 Kota Iwasaki
 # SPDX-License-Identifier: BSD-3-Clause
 
-dir=$(pwd)
-[ -d "$dir/ros2_ws" ] && dir="$dir/ros2_ws"
-
+dir=$(dirname $0)/../..
 cd $dir
 
 source /opt/ros/humble/setup.bash
 
 colcon build
-if [ -f install/setup.bash ]; then
-    source install/setup.bash
-fi
+
+source install/setup.bash
 
 timeout 10 ros2 launch mypkg system_monitor.launch.py > /tmp/mypkg.log
 

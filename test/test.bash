e9ba748609d46e1f2670b35224453433195f0760
Kota Iwasaki <s24C1017db@s.chibakoudai.jp>
2026-01-21 02:57:03 +0900

    change

--- test/test.bash
+++ test/test.bash
@@ -2,14 +2,26 @@
 # SPDX-FileCopyrightText: 2026 Kota Iwasaki
 # SPDX-License-Identifier: BSD-3-Clause
 
-dir=~
-[ "$1" != "" ] && dir="$1"
+dir=$(pwd)
+[ -d "$dir/ros2_ws" ] && dir="$dir/ros2_ws"
 
-cd $dir/ros2_ws
-colcon build
+cd $dir
+
 source /opt/ros/humble/setup.bash
-source $dir/ros2_ws/install/setup.bash
+
+colcon build
+if [ -f install/setup.bash ]; then
+    source install/setup.bash
+fi
+
 timeout 10 ros2 launch mypkg system_monitor.launch.py > /tmp/mypkg.log
 
-cat /tmp/mypkg.log |
-grep 'Listen: 10'
+cat /tmp/mypkg.log
+
+if grep -q 'CPU Usage' /tmp/mypkg.log; then
+    echo "SUCCESS"
+    exit 0
+else
+    echo "FAILED"
+    exit 1
+fi

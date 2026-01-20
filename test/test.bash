f0274a944d77fecdfe6b8b050c9fb5df14a915cd
Kota Iwasaki <s24C1017db@s.chibakoudai.jp>
2026-01-21 02:00:39 +0900

    change

--- test/test.bash
+++ test/test.bash
@@ -7,7 +7,8 @@ dir=~
 
 cd $dir/ros2_ws
 colcon build
-source $dir/.bashrc
+source /opt/ros/humble/setup.bash
+source $dir/ros2_ws/install/setup.bash
 timeout 10 ros2 launch mypkg system_monitor.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
